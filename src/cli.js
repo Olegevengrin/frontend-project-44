bd1e9dec58c22a4abc4de935b5494313f41c608b
Oleg <glushkovolegg@gmail.com>
2023-05-05 00:46:57 +0300

    src

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const questionName = () => {
+export default () => {
+  console.log('Welcome to Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hi ${userName}!`);
 };
