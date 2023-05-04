1131c441796a5fdafa7ed90e9dcfaf434de836f7
Oleg <glushkovolegg@gmail.com>
2023-05-05 00:42:02 +0300

    src

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
-function questionName() {
+export const questionName = () => {
   const userName = readlineSync.question('May I have your name? ');
   return console.log(`Hi ${userName}!`);
-}
-export default { questionName };
+};
